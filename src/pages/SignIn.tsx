eb1923a0e2e46e3e62e1dc48434bb88642c274e1
Israel Teixeira <israeltx@gmail.com>
2025-08-18 16:35:11 -0300

    Create the button component

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,10 +1,12 @@
 import { Input } from "../components/Input"
+import { Button } from "../components/Button"
 
 export function SignIn() {
   return (
     <form action="" className="w-full flex flex-col gap-4">
       <Input required legend="E-mail" type="email" placeholder="seu@email.com"/>
       <Input required legend="Senha" type="password" placeholder="123456"/>
+      <Button>Entrar</Button>
     </form>
   )
 }
