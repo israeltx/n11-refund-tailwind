6d48c7483a03bb6c7678a33a14d1abcf1a1bded0
Israel Teixeira <israeltx@gmail.com>
2025-08-18 16:43:12 -0300

    Create onSubmit function to caputer auth info from the user

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react"
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 
 export function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+
+  function onSubmit(e:React.FocusEvent) {
+    e.preventDefault()
+    console.log(email, password);
+  }
+
   return (
-    <form action="" className="w-full flex flex-col gap-4">
-      <Input required legend="E-mail" type="email" placeholder="seu@email.com"/>
-      <Input required legend="Senha" type="password" placeholder="123456"/>
-      <Button>Entrar</Button>
+    <form onSubmit={onSubmit} action="" className="w-full flex flex-col gap-4">
+      <Input required legend="E-mail" type="email" placeholder="seu@email.com" onChange={(e) => setEmail(e.target.value)}/>
+      <Input required legend="Senha" type="password" placeholder="123456" onChange={(e) => setPassword(e.target.value)}/>
+      <Button type="submit" isLoading={isLoading}>Entrar</Button>
     </form>
   )
 }
