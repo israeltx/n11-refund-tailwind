cec99ae3bf8cbade4b3a59f811dcf838b80281ab
Israel Teixeira <israeltx@gmail.com>
2025-08-19 16:45:09 -0300

    Create the enviar button

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -2,19 +2,30 @@ import { useState } from "react";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
 import { Upload } from "../components/Upload";
+import { Button } from "../components/Button";
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 
 export function Refund() {
+  const [name, setName] = useState('')
+  const [amount, setAmount] = useState('')
   const [category, setCategory] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [filename, setFilename] = useState<File | null>(null)
+
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    console.log(name, amount, category, filename);
+  }
 
   return (
-    <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+    <form onSubmit={onSubmit} className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
       <header>
         <h1 className="text-xl font-bold text-gray-100">Solicitação de reembolso</h1>
         <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesa para solicitar reembolso</p>
       </header>
 
-      <Input required legend="Nome da solicitação"/>
+      <Input required legend="Nome da solicitação" value={name} onChange={(e) => setName(e.target.value)}/>
 
       <div className="flex gap-4">
         <Select required legend="Categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
@@ -25,10 +36,11 @@ export function Refund() {
           }
         </Select>
 
-        <Input required legend="Valor"/>
+        <Input required legend="Valor" value={amount} onChange={(e) => setAmount(e.target.value)}/>
       </div>
 
-      <Upload filename='teste.png'/>
+      <Upload filename={filename && filename.name} onChange={(e) => e.target.files && setFilename(e.target.files[0])}/>
+      <Button type="submit" isLoading={isLoading}>Enviar</Button>
     </form>
   )
 }
