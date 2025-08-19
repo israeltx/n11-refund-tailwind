c73bbd0bd5ba063bc7d6773e70f963d649babdee
Israel Teixeira <israeltx@gmail.com>
2025-08-19 17:07:46 -0300

    Create the confirmation page

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
 import { Upload } from "../components/Upload";
@@ -12,10 +13,13 @@ export function Refund() {
   const [isLoading, setIsLoading] = useState(false)
   const [filename, setFilename] = useState<File | null>(null)
 
+  const navigate = useNavigate()
+
 
   function onSubmit(e: React.FormEvent) {
     e.preventDefault()
     console.log(name, amount, category, filename);
+    navigate('/confirm', {state: {fromSubmit: true}})
   }
 
   return (
