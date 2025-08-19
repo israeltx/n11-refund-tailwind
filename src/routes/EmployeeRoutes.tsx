0c9c1447bcec24fdcd57e6ae9d91ca5fc3c682e2
Israel Teixeira <israeltx@gmail.com>
2025-08-19 12:03:05 -0300

    Create the refund route and page for the employees

---
+++ src/routes/EmployeeRoutes.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router";
+import { Refund } from "../pages/Refund";
+import { NotFound } from "../pages/NotFound";
+
+export function EmployeeRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Refund />}/>
+      <Route path="*" element={<NotFound />}/>
+    </Routes>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route } from "react-router";
 import { AuthRoutes } from "./AuthRoutes";
+import { EmployeeRoutes } from "./EmployeeRoutes";
 
 export function Routes() {
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      <EmployeeRoutes />
     </BrowserRouter>
   )
 }
