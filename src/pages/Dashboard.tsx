0bbb22750e7f6d523cd9de0b9fa72cbfdab5ce10
Israel Teixeira <israeltx@gmail.com>
2025-08-21 14:31:39 -0300

    Create a button variant for searching

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,9 @@
-import React, { use, useState } from "react"
+import React, { useState } from "react"
+
+import searchSvg from "../assets/search.svg"
 
 import { Input } from "../components/Input"
+import { Button } from "../components/Button";
 
 export function Dashboard() {
   const [name, setName] = useState('')
@@ -17,6 +20,7 @@ export function Dashboard() {
 
       <form onSubmit={fetchRefunds} className="flex flex-1 items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6">
         <Input placeholder="Pesquisa pelo nome" onChange={(e) => setName(e.target.value)}/>
+        <Button type="submit" variant="icon"><img src={searchSvg} alt="Ícone de pesquisa" className="w-5" /></Button>
       </form>
     </div>
   )
