a5abae8cc83643cd79871e39e62416cf394199b5
Israel Teixeira <israeltx@gmail.com>
2025-08-22 15:23:03 -0300

    Create routes logic

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,39 @@
-import { BrowserRouter, Route } from "react-router";
 import { AuthRoutes } from "./AuthRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
-import { EmployeeRoutes } from "./EmployeeRoutes";
 import { Loading } from "../components/Loading";
+import { EmployeeRoutes } from "./EmployeeRoutes";
+import { BrowserRouter, Route } from "react-router";
 
 const isLoading = false
+// Test if no user is logged in
+// const session = undefined
+
+const session = {
+  user: {
+    role: 'employee'
+  }
+}
 
 export function Routes() {
+  function Route() {
+    switch (session?.user.role) {
+      case 'employee':
+        return <EmployeeRoutes />
+      case 'manager':
+        return <ManagerRoutes />
+      default:
+        return <AuthRoutes />
+    }
+  }
+
+
   if (isLoading) {
     return <Loading />
   }
 
-
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      <Route />
     </BrowserRouter>
   )
 }
