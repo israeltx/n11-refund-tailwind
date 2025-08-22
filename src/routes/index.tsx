af93ffa777dff40a00fe01fe2c76265081d7714c
Israel Teixeira <israeltx@gmail.com>
2025-08-22 15:13:10 -0300

    Create the loading component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Route } from "react-router";
-// import { AuthRoutes } from "./AuthRoutes";
+import { AuthRoutes } from "./AuthRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
-// import { EmployeeRoutes } from "./EmployeeRoutes";
+import { EmployeeRoutes } from "./EmployeeRoutes";
+import { Loading } from "../components/Loading";
+
+const isLoading = false
 
 export function Routes() {
+  if (isLoading) {
+    return <Loading />
+  }
+
+
   return (
     <BrowserRouter>
-      <ManagerRoutes />
+      <AuthRoutes />
     </BrowserRouter>
   )
 }
