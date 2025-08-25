f3c68f77396b5b0e17e21e03943053cbfdddcbf6
Israel Teixeira <israeltx@gmail.com>
2025-08-25 12:59:05 -0300

    Implement validation using zod in the SignIn page

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,32 +1,39 @@
 import { useActionState } from "react"
+import { email, z, ZodError } from "zod";
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
-import { email } from "zod";
+
+const signInSchema = z.object({
+  email: z.email({message: "E-mail inválido"}),
+  password: z.string().trim().min(1, {message: "Informe a senha"})
+})
 
 export function SignIn() {
-  const [state, formAction, isLoading] = useActionState(signIn, {
-    email: "",
-    password: ""
-  })
+  const [state, formAction, isLoading] = useActionState(signIn, null)
 
-  async function signIn(prevState: any, formData: FormData) {
-    const email = formData.get("email")
-    const password = formData.get("password")
-
-    return {email, password}
-
-    // Simulate API response
-    // await new Promise((resolve) => {
-    //   setTimeout(() => {
-    //     resolve("Ok")
-    //   }, 2000)
-    // })
+  async function signIn(_: any, formData: FormData) {
+    try {
+      const data = signInSchema.parse({
+        email: formData.get('email'),
+        password: formData.get('password')
+      })     
+      console.log(data); 
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return {message: error.issues[0].message}
+      }
+      return {message: "Não foi possível entrar"}
+    }
   }
 
   return (
     <form action={formAction} className="w-full flex flex-col gap-4">
       <Input required name="email" legend="E-mail" type="email" placeholder="seu@email.com" />
       <Input required name="password" legend="Senha" type="password" placeholder="123456" />
+
+      <p className="text-sm text-red-600 text-center my-4 font-medium">
+        {state?.message}
+      </p>
       
       <Button type="submit" isLoading={isLoading}>Entrar</Button>
 
