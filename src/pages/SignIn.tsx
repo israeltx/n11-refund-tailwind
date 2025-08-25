c0eeb2d69a924f39ce950c844579788d2eae8607
Israel Teixeira <israeltx@gmail.com>
2025-08-25 12:46:05 -0300

    Implement useActionState

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,16 +1,30 @@
-import { useState } from "react"
+import { useActionState } from "react"
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
+import { email } from "zod";
 
 export function SignIn() {
-  const [isLoading, setIsLoading] = useState(false)
+  const [state, formAction, isLoading] = useActionState(signIn, {
+    email: "",
+    password: ""
+  })
 
-  function onAction(formData: FormData) {
-    console.log(formData.get("email"));
+  async function signIn(prevState: any, formData: FormData) {
+    const email = formData.get("email")
+    const password = formData.get("password")
+
+    return {email, password}
+
+    // Simulate API response
+    // await new Promise((resolve) => {
+    //   setTimeout(() => {
+    //     resolve("Ok")
+    //   }, 2000)
+    // })
   }
 
   return (
-    <form action={onAction} className="w-full flex flex-col gap-4">
+    <form action={formAction} className="w-full flex flex-col gap-4">
       <Input required name="email" legend="E-mail" type="email" placeholder="seu@email.com" />
       <Input required name="password" legend="Senha" type="password" placeholder="123456" />
       
